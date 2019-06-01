98566cc88ce51e742827b6863a9337c85f9b1739
ulysse31 <ulysse31@gmail.com>
2019-06-01 23:20:29 +0200

    removed old lib commented code

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,4 @@
 #include <Arduino.h>
-
-/*
-#include <SPI.h>
-#include <mcp2515.h>
-
-# define CAN_CS   5
-# define LED      17
-
-struct can_frame frametable[] =
-{
-  {0x33a, 5, {0x30, 0x82, 0x81, 0x0a, 0x7f, 0, 0, 0}},
-  {0x33a, 5, {0x30, 0x82, 0x81, 0x0a, 0x7f, 0, 0, 0}},
-  {0x4f3, 8, {0x78, 0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff}},
-  {0x4f3, 8, {0x78, 0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff}},
-  {0x4f3, 8, {0x78, 0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff}},
-  {0x4f3, 8, {0x78, 0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff}},
-  {0x4f3, 8, {0x78, 0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff}},
-  {0x4f3, 8, {0x78, 0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff}},
-  {0x33a, 5, {0x30, 0x82, 0x81, 0x0a, 0x7f, 0, 0, 0}},
-  {0, 0, {0, 0, 0, 0, 0, 0, 0, 0}}
-};
-
-
-MCP2515 mcp2515(CAN_CS);
-
-
-void setup()
-{
-//  while (!Serial);
-  Serial.begin(115200);
-  SPI.begin();
-  
-  mcp2515.reset();
-  mcp2515.setBitrate(CAN_100KBPS, MCP_8MHZ);
-  mcp2515.setNormalMode();
-  pinMode(LED, OUTPUT);
-}
-
-void loop()
-{  
-  int i;
-  digitalWrite(LED, LOW);
-  for (i = 0; frametable[i].can_id != 0; i++)
-  {
-    mcp2515.sendMessage(&(frametable[i]));
-    digitalWrite(LED, (i % 2 ? HIGH : LOW));
-    delay (100);
-  }
-  delay(2000);
-}
-*/
-
 #include <mcp_can.h>
 #include <SPI.h>
 
@@ -81,7 +29,7 @@ t_canpacket packettable[] =
 };
 
 
-MCP_CAN CAN0(CAN_CS);     // Set CS to pin 10
+MCP_CAN CANDEV(CAN_CS);
 
 void Error_loop(uint8_t count, uint8_t time)
 {
@@ -101,9 +49,9 @@ void setup()
 {
   pinMode(LED, OUTPUT);
 
-  while (!(CAN0.begin(MCP_ANY, CAN_100KBPS, MCP_8MHZ) == CAN_OK))
+  while (!(CANDEV.begin(MCP_ANY, CAN_100KBPS, MCP_8MHZ) == CAN_OK))
     Error_loop(5, 2000);
-  CAN0.setMode(MCP_NORMAL);   // Change to normal mode to allow messages to be transmitted
+  CANDEV.setMode(MCP_NORMAL);   // Change to normal mode to allow messages to be transmitted
 }
 
 void loop()
@@ -114,7 +62,7 @@ void loop()
   digitalWrite(LED, LOW);
   for (timeout = 0, i = 0; packettable[i].id != 0 && timeout < MAX_TX_ERRS; i++)
   {
-    byte sndStat = CAN0.sendMsgBuf(packettable[i].id, packettable[i].ext, packettable[i].len, packettable[i].data);
+    byte sndStat = CANDEV.sendMsgBuf(packettable[i].id, packettable[i].ext, packettable[i].len, packettable[i].data);
     if (sndStat == CAN_OK)
       digitalWrite(LED, (i % 2 ? HIGH : LOW));
     else
