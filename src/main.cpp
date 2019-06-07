1303efa8db3c720eb68a93b32efd80a553415a19
ulysse31 <ulysse31@gmail.com>
2019-06-07 21:04:29 +0200

    Had more time to really code at last one hour of coding and debugging: - corrected a dirty error on conditions - removed useless tests, variables and piece of code by changing a bit the algorithm - optimized code, speed and performance
    ... what else ?! :p

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,6 @@ typedef struct      canpacket
 
 typedef struct	framecycle
 {
-  bool		firstrun;
   unsigned long	start;
   unsigned long	cycletime;
   t_canpacket	*packet;
@@ -25,29 +24,29 @@ typedef struct	framecycle
 t_canpacket status = {0x33a, 5, {0x30, 0x82, 0x81, 0x0a, 0x7f, 0, 0, 0}};
 t_canpacket network = {0x4f3, 8, {0x78, 0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff}};
 
-/*
+
 // PROD VALUES
 t_framecycle	framecycletable[] = 
   {
-    {true, 0, 10030, &status},
-    {true, 160, 10030, &status},
-    {true, 380, 10030, &status},
-    {true, 0, 940, &network},
-    {true, 0, 0, 0}
+    {0, 10030, &status},
+    {160, 10030, &status},
+    {380, 10030, &status},
+    {0, 940, &network},
+    {0, 0, 0}
   };
-*/
 
+/* 
 // DEBUG VALUES
 t_framecycle	framecycletable[] = 
   {
-    {true, 0, 10030, &status},
-    {true, 1600, 10030, &status},
-    {true, 3800, 10030, &status},
-    {true, 0, 9400, &network},
-    {true, 0, 0, NULL}
+    {0, 10030, &status},
+    {1600, 10030, &status},
+    {3800, 10030, &status},
+    {0, 9400, &network},
+    {0, 0, NULL}
   };
+*/
 
-unsigned long inittime;
 MCP_CAN CANDEV(CAN_CS);
 
 void Error_loop(uint8_t count, uint16_t time)
@@ -68,42 +67,25 @@ bool		send_packet(INT32U id, INT8U len, INT8U *data)
 {
   uint8_t	timeout = 0;
 
-  while (timeout <= MAX_TX_ERRS)
-    {
-      byte sndStat = CANDEV.sendMsgBuf(id, len, data);
-
-      if(sndStat == CAN_OK)
-	{
-	  Serial.println("Message Sent Successfully!");
-	  break ;
-	}
-      else
-	{
-	  Serial.println("Error Sending Message... Retrying ...");
-	  timeout++;
-	}
-    }
+  while (CANDEV.sendMsgBuf(id, len, data) != CAN_OK && timeout <= MAX_TX_ERRS)
+     timeout++;
   if (timeout >= MAX_TX_ERRS)
-    {
-      Serial.println("Giving up this packet, going to next one");
-      return (false);
-    }
+    return (false);
   else
     return (true);
 }
 
-void setup()
+void            setup()
 {
-  pinMode(LED, OUTPUT);
-  Serial.begin(115200);
+  unsigned long inittime;
 
+  pinMode(LED, OUTPUT);
   while (!(CANDEV.begin(MCP_ANY, CAN_100KBPS, MCP_8MHZ) == CAN_OK))
-    {
-      Error_loop(5, 2000);
-      Serial.println("Error Initializing CAN DEVICE...");
-    }
+    Error_loop(5, 2000);
   CANDEV.setMode(MCP_NORMAL);
   inittime = millis();
+  for (uint8_t i = 0; framecycletable[i].packet != NULL; i++)
+    framecycletable[i].start += inittime;
 }
 
 void loop()
@@ -112,24 +94,11 @@ void loop()
   
   digitalWrite(LED, LOW);
   for (i = 0; framecycletable[i].packet != NULL; i++)
-    if (framecycletable[i].firstrun == true)
-      {
-	if ((inittime + framecycletable[i].start) >= millis())
-	  if (send_packet(framecycletable[i].packet->id, framecycletable[i].packet->len, framecycletable[i].packet->data) == true)
-	      {
-		framecycletable[i].start = millis();
-		framecycletable[i].firstrun = false;
-		digitalWrite(LED, (i % 2 ? LOW : HIGH));
-	      }
-      }
-    else
-      {
-	if ((framecycletable[i].start + framecycletable[i].cycletime) >= millis())
-	  if (send_packet(framecycletable[i].packet->id, framecycletable[i].packet->len, framecycletable[i].packet->data) == true)
-	    {
-	      framecycletable[i].start = millis();
-	      digitalWrite(LED, (i % 2 ? LOW : HIGH));
-	    }
-      }
+    if ((framecycletable[i].start + framecycletable[i].cycletime) <= millis())
+      if (send_packet(framecycletable[i].packet->id, framecycletable[i].packet->len, framecycletable[i].packet->data) == true)
+        {
+          framecycletable[i].start = framecycletable[i].start + framecycletable[i].cycletime;
+          digitalWrite(LED, (i % 2 ? LOW : HIGH));
+        }
   delay(5);
 }
